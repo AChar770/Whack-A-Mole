f673476f345f5f0825750c85ed70f9ff7efe2f8e
AChar770 <amandatcharles@yahoo.com>
2026-02-11 22:46:41 -0500

    provider, state, and functions established

--- components/GameContext.jsx
+++ components/GameContext.jsx
@@ -1,1 +1,24 @@
 import { createContext, useContext, useState } from "react";
+const GameContext = createContext()
+export function GameProvider({ children }) {
+    const [score, setScore] = useState(0);
+    const [molePosition, setMolePosition] = useState(2);
+    const [isPlaying, setIsPlaying] = useState(false);
+}
+
+function whackMole() {
+    setScore(score + 1);
+    setMolePosition(Math.floor(Math.random() * 9));
+}
+
+function startGame() {
+    setIsPlaying(true);
+    setScore(0);
+    setMolePosition(Math.floor(Math.random() * 9));
+}
+
+function restartGame() {
+    setIsPlaying(false);
+    setScore(0);
+    setMolePosition(Math.floor(Math.random() * 9));
+}
